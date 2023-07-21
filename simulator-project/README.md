ffe27641d151e9062329430d5612a9f3bc4b4803
Jo KyungHyeon <khcho0125@icloud.com>
2023-07-21 20:06:52 +0900

    ➕ :: 설정 파일 추가

--- simulator-project/README.md
+++ simulator-project/README.md
@@ -5,12 +5,14 @@
 
 ### 2. 사용 기술
 
-> ### OpenFeign을 사용한 HTTP 통신
-> 
-> REST 기반 요청을 지원하는 RestTemplate과 비교했을 때, 보다 가독성이 좋고 테스트가 용이하다는 점에서 채택
+#### OpenFeign을 사용한 HTTP 통신
+* REST 기반 요청을 지원하는 RestTemplate과 비교했을 때, 보다 가독성이 좋고 테스트가 용이하다는 점에서 채택
 
-> ### Quartz 동적 스케줄링
-> 
-> 사용자가 작성한 요청을 원하는 시간에 호출하기 위해 사용
+#### Quartz 동적 스케줄링
+* 사용자가 작성한 요청을 원하는 시간에 호출하기 위해 사용
 
-> ### WebSocket을 활용한 실시간 모니터링
+#### WebSocket을 활용한 실시간 모니터링
+
+### 3. 개체 관계 다이어그램
+
+[**권한이 필요합니다.**](https://www.erdcloud.com/d/ccpMfyc3x4fevdbdL)
--- simulator-project/build.gradle.kts
+++ simulator-project/build.gradle.kts
@@ -2,12 +2,15 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
-val h2_version: String by project
+val hikari_version: String by project
+val koin_version: String by project
+val mysql_version: String by project
+
 
 plugins {
     kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
     id("io.ktor.plugin") version "2.3.2"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
 
 group = "com.simsim"
@@ -26,20 +29,26 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
-    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-client-apache-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
+
     implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
+    implementation("com.mysql:mysql-connector-j:$mysql_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+
     implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-cio-jvm:2.3.2")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.2")
+    implementation("io.ktor:ktor-serialization-gson:2.3.2")
+    implementation("io.ktor:ktor-client-logging-jvm:2.3.2")
+
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
